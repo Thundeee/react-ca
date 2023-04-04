c11101adc0781397e7b2742b69e8864ec0e8e792
Thundeee <asbjornabel@gmail.com>
2023-04-04 16:24:52 +0200

    added footer styling

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,12 +1,17 @@
 import { Typography } from "@mui/material";
+import { useTheme } from '@mui/material/styles';
+import {
+  FooterWrapper,
 
+} from './styles';
 const Footer = () => {
+  const theme = useTheme();
   return (
-    <footer>
+    <FooterWrapper theme={theme}>
       <Typography variant="h6" align="center" gutterBottom>
     @ {new Date().getFullYear()} - QuickShop
       </Typography>
-    </footer>
+    </FooterWrapper>
   );
 };
 
